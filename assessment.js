4d815617078a80d7bc31a0cd6887953b4343ffaf
satoki550 <62322491+satoki550@users.noreply.github.com>
2020-03-23 13:12:53 +0900

    Add files via upload

---
+++ assessment.js
@@ -0,0 +1,88 @@
+'use strict' ;
+const userNameInput = document.getElementById('user-name');
+const assessmentButton = document.getElementById('assessment');
+const resultDivided = document.getElementById('result-area');
+const tweetDivided = document.getElementById('tweet-area');
+
+function removeALLChildren(element) {
+    while (element.firstChild) {
+    element.removeChild(element.firstChild);
+    }
+}
+
+assessmentButton.onclick = () => {
+    const userName = userNameInput.value;
+    if (userName.length === 0) {
+        return;
+    }
+    console.log(userName);
+    
+    
+    removeALLChildren(resultDivided);
+    const header = document.createElement('h3');
+    header.innerText = '診断結果';
+    resultDivided.appendChild(header);
+
+    const paragraph = document.createElement('p');
+    const result = assessment(userName);
+    paragraph.innerText = result;
+    resultDivided.appendChild(paragraph);
+
+    removeALLChildren(tweetDivided);
+    const anchor = document.createElement('a');
+    const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag=' + encodeURIComponent('あなたのいいところ') + '&ref_src=twsrc%5Etfw';
+
+    anchor.setAttribute('href', hrefValue);
+    anchor.className = 'twitter-hashtag-button';
+    anchor.setAttribute('data-text', result);
+    anchor.innerText = 'Tweet #あなたのいいところ';
+
+    tweetDivided.appendChild(anchor);
+
+    const script = document.createElement('script');
+    script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
+    tweetDivided.appendChild(script);
+
+    
+};
+userNameInput.onkeydown = (event) => {
+    if (event.key === 'Enter'){
+        assessmentButton.onclick();
+    }
+};
+const answers = [
+    '{userName}のいいところは声です。',
+    '{userName}のいいところは眼差しです。',
+    '{userName}のいいところは情熱です。',
+    '{userName}のいいところは厳しさです。',
+    '{userName}のいいところは知識です。',
+    '{userName}のいいところはユニークさです。',
+    '{userName}のいいところは用心深さです。',
+    '{userName}のいいところは見た目です。',
+    '{userName}のいいところは決断力です。',
+    '{userName}のいいところは思いやりです。',
+    '{userName}のいいところは感受性です。',
+    '{userName}のいいところは節度です。',
+    '{userName}のいいところは好奇心です。',
+    '{userName}のいいところは気配りです。',
+    '{userName}のいいところはその全てです。',
+    '{userNmae}のいいところは自制心です。'
+];
+function assessment(userName) {
+    //全文字のコード番号を取得して足し合わせる
+    let sumOfCharCode = 0;
+    for (let i = 0; i < userName.length; i++) {
+        sumOfCharCode = sumOfCharCode + userName.charCodeAt(i);
+    }
+
+    //文字のコード番号の合計を回答の数で割って添字の数値を求める
+    const index = sumOfCharCode % answers.length;
+    let result = answers[index];
+    result = result.replace(/\{userName}/g, userName);
+
+    return result;
+}
+console.assert(
+    assessment('太郎')　=== '太郎のいいところは決断力です。',
+    '診断結果の文言を名前に置き換える処理が正しくありません。'
+);
